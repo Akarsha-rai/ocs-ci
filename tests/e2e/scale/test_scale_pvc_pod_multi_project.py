5b7df6ebebb7efdecc0734035b76fc592ccb58b0
Akarsha <akrai@redhat.com>
2020-03-27 17:56:20 +0530

    Remove sa
    Signed-off-by: Akarsha <akrai@redhat.com>

--- tests/e2e/scale/test_scale_pvc_pod_multi_project.py
+++ tests/e2e/scale/test_scale_pvc_pod_multi_project.py
@@ -76,7 +76,6 @@ class TestMultiProjectScalePVCPOD(E2ETest):
 
         # Create namespace
         self.namespace_list.append(helpers.create_project())
-        namespace = (self.namespace_list[-1]).namespace
 
         # Continue to iterate till the scale pvc limit is reached
         while True:
@@ -84,11 +83,12 @@ class TestMultiProjectScalePVCPOD(E2ETest):
                 log.info(f"Scaled {scale_count} pvc and pods")
                 break
             else:
-                sa_name = helpers.create_serviceaccount(namespace)
+                # namespace = (self.namespace_list[-1]).namespace
+                # sa_name = helpers.create_serviceaccount(namespace)
                 log.info(f"Create {pvc_count_each_itr} pods & pvc")
                 pod_obj, pvc_obj = helpers.create_multi_pvc_pod(
                     (self.namespace_list[-1]).namespace, rbd_sc_obj, cephfs_sc_obj, pvc_count_each_itr,
-                    size, sa_name.name, dc_deployment=True, fio_rate=fio_rate_param, start_io=start_io, fio_size=fio_size_param,
+                    size, fio_rate=fio_rate_param, start_io=start_io, fio_size=fio_size_param,
                     fio_runtime=360000, node_selector=constants.SCALE_NODE_SELECTOR
                 )
                 all_pod_obj.extend(pod_obj)
